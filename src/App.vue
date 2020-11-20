8343334c6b751c8b706e28a4c2e29726e3e37263
Jude <benedict.jude.rocat@gmail.com>
2020-11-21 00:49:36 +0800

    ➕ ADD autologin, settings persistence

--- src/App.vue
+++ src/App.vue
@@ -9,9 +9,19 @@
 </template>
 
 <script lang="ts">
+import path from 'path';
+import fs from 'fs';
+
 import Vue from 'vue';
 import sharedStore from './store';
+
 import {ELearn} from './elearn';
+import {remote} from 'electron';
+
+const data = remote.app.getPath("appData");
+let endeavor;
+
+console.log("Data path: " + data);
 
 export default Vue.extend({
   data() {
@@ -19,12 +29,31 @@ export default Vue.extend({
       sharedStore
     };
   },
-  created () {
+  async created () {
     // Creating new eLearn object in store...
     this.sharedStore.eLearn = new ELearn();
     this.$router.push('/login');
+    
+    // Checking if endeavor.json file exists
+    if (fs.existsSync(path.join(data, "endeavor.json"))) {
+      endeavor = JSON.parse(fs.readFileSync(path.join(data, "endeavor.json"), {encoding: "utf8"}));
+      try {
+        this.sharedStore.settings = endeavor.settings;
+        if (this.sharedStore.settings.saveLogin) {
+          const loginResult = await this.sharedStore.eLearn.login(endeavor.username, endeavor.password);
+          if (loginResult) {
+            console.log("Autologin success");
+            this.$router.push("/home");
+          } else {
+            console.warn("Automatic login failed!");
+          }
+        }
+      } catch(err) {
+        console.warn("An error occurred trying to initialize app preferences." + err);
+      }
+    }
   }
-})
+});
 
 </script>
 
