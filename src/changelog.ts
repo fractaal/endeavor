8e1ebab346b29216c270634f55af7a9d85c30b48
Jude <benedict.jude.rocat@gmail.com>
2020-11-21 12:38:36 +0800

    ➕ Add changelog.ts file for ChangeLogTicker.vue

---
+++ src/changelog.ts
@@ -0,0 +1,18 @@
+export default [
+  {
+    title: "✨ Secure automatic login!",
+    text: "The automatic login feature now uses your secure & native OS credential manager."
+  },
+  {
+    title: "❌ Removed broken buttons",
+    text: "In modules with file attachments, the 🔍 button would lead you to a blank screen. This has been removed for now!"
+  },
+  {
+    title: "🖼 Layout changes",
+    text: "Layout changes to the login screen and the automatic login screen (new) were made!"
+  },
+  {
+    title: "🐜 Bug fix!",
+    text: "Just one bug fix. And it was about the easter egg you had to find. Oh well."
+  },
+]
