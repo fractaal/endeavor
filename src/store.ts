ffc46d2f96b48cd633f3d69fa096d155a6a5a668
Jude <benedict.jude.rocat@gmail.com>
2020-11-30 21:14:32 +0800

    🌀 Refactor caching, move to elearn/index.ts

--- src/store.ts
+++ src/store.ts
@@ -1,52 +1,32 @@
-import {Course, eLearnInterface} from './interfaces';
+import {eLearnInterface} from './interfaces/eLearn';
+import fuseSearch from './search';
 
 interface Store {
   eLearn: eLearnInterface|null;
-  cache: {
-    courses: Course[];
-  };
   search: string;
+  theme: string;
+  searchResults: [];
   settings: {
     showDebugInfo: boolean;
     saveLogin: boolean;
   };
+  searchFunction: Function;
+  searchTimer: any;
+  userDoneTypingOnSearch: boolean;
 }
 
-const store = {
+const store: Store = {
   eLearn: null,
-  cache: {
-    courses: [],
-  },
   search: '',
+  theme: "light-theme",
+  searchResults: [],
   settings: {
     showDebugInfo: false,
     saveLogin: false,
   },
+  searchFunction: fuseSearch,
+  searchTimer: null,
+  userDoneTypingOnSearch: true,
 };
-
-export function addCourseToCache(newCourse: Course): boolean {
-  for (const course of store.cache.courses) {
-    if (course.id == newCourse.id) return true;
-  }
-  store.cache.courses.push(newCourse); 
-}
-
-export function updateCourseInCache(updatedCourse: Course): boolean {
-  for (let course of store.cache.courses) {
-    if (course.id == updatedCourse.id) {
-      course = updatedCourse;
-      return true;
-    }
-  }
-  return false;
-}
-
-export function getCourseFromCache(id: string|number): Course {
-  for (const course of store.cache.courses) {
-    if (course.id == id) {
-      return course;
-    }
-  }
-}
 
 export default store;
