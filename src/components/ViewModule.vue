8bf80ee9e5d8a3453580931199359eff80f41ee4
Jude <benedict.jude.rocat@gmail.com>
2020-11-30 21:17:26 +0800

    ✨ Remove business logic, Implement Card component

--- src/components/ViewModule.vue
+++ src/components/ViewModule.vue
@@ -1,21 +1,10 @@
 <template>
   <div>
-    <div style="margin-left: 50px; margin-top: 25px; margin-bottom: 10px;">
+    <loader v-if="!module" :text="`Loading module`"/>
+    <div v-else style="margin-left: 50px; margin-top: 25px; margin-bottom: 10px;">
       <h1 class="nospacing">{{module.name}}</h1>
       <div style="display: flex; justify-content: space-  between;">
         <div>
-          <div :class="module.classOverride" class="badge" v-if="module.duedateFormatted">
-            <p class="nospacing">Due {{module.duedateFormatted}}</p>
-          </div>
-          <div class="badge" v-if="module.timeopenFormatted">
-            <p class="nospacing">{{module.timeopenFormatted}}</p>
-          </div>
-          <div class="badge" v-if="module.timecloseFormatted">
-            <p class="nospacing">{{module.timecloseFormatted}}</p>
-          </div>
-          <div class="badge" v-if="module.timelimitFormatted">
-            <p class="nospacing">Due {{module.timelimitFormatted}}</p>
-          </div>
           <p class="nospacing">{{module['modname']}}</p>
         </div>
         <div style="margin-left: 50px; margin-right: 50px;">
@@ -25,28 +14,6 @@
         </div>
       </div>
     </div>
-    <br>
-    <hr>
-    <div style="overflow-y: auto; max-height: 69vh;">
-      <transition name="transition">
-        <div style="margin-left: 50px; margin-right: 50px;">
-          <p v-html="module.description"></p>
-          <div v-if="module.contents || module.introattachments">
-            <div v-for="item in module.introattachments" :key="item.filename">
-              <Card :title="item.filename"/>
-            </div>
-            <div v-for="item in module.contents" :key="item.filename">
-              <Card :title="item.filename"/>
-            </div>
-          </div>
-          <pre v-if="sharedStore.settings.showDebugInfo" style="background-color: black; padding: 10px;">
-            <h2 class="nospacing">COMPLICATED DEBUG INFORMATION</h2>
-            {{JSON.stringify(module, 0, 2)}}  
-          </pre>
-        </div>
-      </transition>
-      <Loader v-if="isLoading"/>
-    </div>
   </div>
 </template>
 
@@ -54,14 +21,14 @@
 import sharedStore, { addCourseToCache, getCourseFromCache } from '../store';
 import {formatDistance, format} from 'date-fns';
 
-import Card from './Card';
-import Loader from './Loader';
+import Card from './Card.vue';
+import Loader from './Loader.vue';
 
 export default {
   name: "ViewModule",
   components: {
     Loader,
-    Card,
+    // Card
   },
   data() {
     return {
@@ -72,51 +39,13 @@ export default {
   },
   beforeRouteEnter(to, from, next) {
     next(vm => {
-      vm.getModuleData();
+      vm.getModule()
     })
   },
   methods: {
-    async getModuleData() {
-      this.isLoading = true;
-      this.module = {};
-      /**
-       * Add the course to the cache.
-       */
-      let course = getCourseFromCache(this.$route.params.course);
-      let courseMeta;
-      if (!course) {
-        const courseMetas = await sharedStore.eLearn.getCourses();
-        for (const course of courseMetas) {
-          if (course.id == this.$route.params.course) {
-            courseMeta = course;
-            courseMeta.info = await sharedStore.eLearn.getCourseInfo(this.$route.params.course);
-            break;
-          }
-        }
-        course = courseMeta;
-        addCourseToCache(courseMeta);
-      }
-
-      for (const section of course.info) {
-        for (const module of section.modules) {
-          if (module.instance == this.$route.params.instance || module.cmid == this.$route.params.instance || module.id == this.$route.params.instance || module.coursemodule == this.$route.params.instance) {
-            this.module = module;
-          }
-        }
-      }
-
-      this.format();
-      this.isLoading = false;
-    },
-
-    format() {
-       try {
-         console.log(this.module)
-         this.module.duedateFormatted = formatDistance(this.module.duedate, new Date(), {addSuffix: true});
-       } catch(e) {
-         console.warn("Formatting data for this module failed: " + e);
-         console.log(this.module.duedate);
-       }
+    async getModule() {
+      this.module = await this.sharedStore.eLearn.getModule(this.$route.params.course, this.$route.params.instance);
+      console.log(this.module);
     }
   }
 }
--- src/components/ViewSection.vue
+++ src/components/ViewSection.vue
@@ -1,145 +1,55 @@
 <template>
   <div>
-    <h1 style="margin-left: 50px;">{{section.name}}</h1>
-    <div style="overflow-y: auto; max-height: 80vh; margin-left: 25px; margin-right: 25px;">
-      <transition-group name="transition">
-        <div v-for="module in filteredModules" :key="module.id">
-          <div class="card" style="border-radius: 10px 10px 0px 0px; margin-bottom: 0;">
-            <div style="display: flex; justify-content: space-between;">
-              <div style="display: flex;">
-                <p style="font-weight: 800; margin: 0;">{{module.name}}</p>
-              </div>
-              <div style="display: flex;">
-                <p style="margin:0; font-weight: 400;">{{module.duedateFormatted}}</p>
-              </div>
-            </div>
-          </div>
-          <div class="card cardDarker" style="border-radius: 0px 0px 10px 10px; margin-top: 0;">
-            <div style="display: flex; justify-content: space-between;">
-              <div style="display: flex;">
-                <p style="margin: 0;" v-html="module.intro"></p>
-              </div>
-              <div style="display: flex;">
-                <button @click="$router.push(`/modules/${course.id}/${module.instance}`)" class="roundButton">🔍</button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </transition-group>
-      <Loader v-if="isLoading"/>
+    <Loader v-if="!section"/>
+    <div v-else>
+      <h1 style="margin-left: 50px;">{{section.name}}</h1>
+      <div class="cardlist">
+        <transition-group name="transition" style="margin-right: 25px; margin-left: 25px;">
+          <card v-for="module in section.modules" :key="module.id"
+            :title="module.name"
+            :subtitle="module.modname"
+            :rightTitle="module.duedateformatted"
+            :rightSubtitle="module.duedatedistanceformatted"
+            :content="module.description"
+            :internalLink="`/modules/${$route.params.id}/${module.instance}`"
+            :externalLink="module.url"
+            :styling="module.styling"
+            />
+        </transition-group>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import sharedStore, { getCourseFromCache } from '../store';
-import {formatDistance, format, formatDistanceStrict} from 'date-fns';
+import sharedStore from '../store';
 
-import Loader from './Loader';
-import { formatDuration } from 'date-fns/fp';
+import Card from './Card.vue';
+import Loader from './Loader.vue';
 
 export default {
   name: "ViewSection",
-  components: {
-    Loader
-  },
   data() {
     return {
       sharedStore,
-      course: {},
       section: {},
       isLoading: false,
     }
   },
-  computed: {
-    filteredModules: function() {
-      const term = this.sharedStore.search.toLowerCase();
-      if (!this.section.modules) return; // Prevent from running if the object doesn't exist yet
-      return this.section.modules.filter(element => {
-        try {
-          if (
-            // Rules to change
-            element.name.toLowerCase().includes(term) ||
-            element.intro.toLowerCase().includes(term)
-          ) return element;
-        } catch(err) {
-          console.warn("!!!");
-        }
-      })
-    }
+  components: {
+    Loader,
+    Card,
   },
   beforeRouteEnter(to, from, next) {
     next(vm => {
-      vm.getSectionData();
+      vm.getSection();
     })
   },
   methods: {
-    async getSectionData() {
-      this.isLoading = true;
-      this.course = {};
-      /**
-       * Add the course to the cache.
-       */
-      const course = getCourseFromCache(this.$route.params.id);
-      this.course = course;
-      for (const section of course.info) {
-        console.log(section);
-        if (section.section == this.$route.params.section) {
-          this.section = section;
-        }
-      }
-
-      this.format();
-      this.isLoading = false;
-    },
-
-    format() {
-      for (const module of this.section.modules) {
-        // Time formatting for forums
-        try {
-          module.duedateFormatted = "Due " + formatDistance(module.duedate, new Date(), {addSuffix: true}) + " - " + format(module.duedate, 'MMMM dd, yyyy');
-        } catch(e) {
-          console.warn("Error formatting due date data. This module might not have a due date." + e);
-        }
-
-        if (!module.intro) {
-          module.intro = "<i>No summary</i>";
-        }
-
-        // Time formatting for quizzes
-        try {
-          module.timeopenFormatted = `🟢 Opens ${format(module.timeopen, "hh:mmaaa, MMMM d, yyyy")}`
-          module.timecloseFormatted = `❌ Closes ${format(module.timeclose, "hh:mmaaa, MMMM d, yyyy")}`
-          module.timelimitFormatted = `⌛ Good for ${formatDistanceStrict(Date.now(), (Date.now()-(module.timelimit*1000)))}`
-        } catch(e) {
-          console.warn("Failed formatting quiz time data. This module might not be a quiz." + e);
-        }
-
-        // Checking if overdue
-        try {
-          if ((new Date() - module.duedate) > 0) {
-            module.isOverdue = true;
-            module.classOverride = "cardReallyDanger";
-          } else {
-            module.isOverude = false;
-          }
-        } catch(err) {
-          console.warn("Failed to check if date was overdue");
-        }
-
-        try {
-          if ((new Date() - module.timeclose) > 0) {
-            module.isOverdue = true;
-            module.classOverride = "cardReallyDanger";
-          } else {
-            module.isOverude = false;
-          }
-        } catch(err) {
-          console.warn("Failed to check if date was overdue");
-        }
-      }
+    async getSection() {
+      this.section = await this.sharedStore.eLearn.getSection(this.$route.params.id, this.$route.params.section);
     }
-  },
+  }
 }
 </script>
 
