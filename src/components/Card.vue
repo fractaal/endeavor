39102b56cf768f2309adca9f95da47a25dac8e31
Jude <benedict.jude.rocat@gmail.com>
2020-11-30 21:15:57 +0800

    ✨Implement Card component on all card UI elements

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,23 +1,36 @@
 <template>
-  <div>
-    <div class="card" style="border-radius: 10px 10px 0px 0px; margin-bottom: 0;">
+  <div :class="styling">
+    <div class="cardUpper" style="border-radius: 10px 10px 0px 0px; margin-bottom: 0;">
       <div style="display: flex; justify-content: space-between;">
-        <div style="display: flex;">
+        <div style="display: flex; flex-direction: column;">
           <p style="font-weight: 800; margin: 0;">{{title}}</p>
-        </div>
-        <div style="display: flex;">
           <p style="margin:0; font-weight: 400;">{{subtitle}}</p>
+        </div>
+        <div>
+          <div style="display: flex; flex-direction: column;">
+            <p style="font-weight: 800; margin: 0;">{{rightTitle}}</p>
+            <p style="margin:0; font-weight: 400;">{{rightSubtitle}}</p>
+          </div>
+          <div style="display: flex;">
+            <div v-if="externalLink" style="margin-right: 20px; display: flex; flex-direction: column; align-items: center;">
+              <button @click="openExternalLink" class="roundButton">🔬</button>
+              <p class="nospacing">Open in eLearn...</p>
+            </div>
+            <div v-if="internalLink" style="display: flex; flex-direction: column; align-items: center;">
+              <button @click="$router.push(internalLink)" class="roundButton">🔍</button>
+              <p class="nospacing">Open...</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
-    <div class="card cardDarker" style="border-radius: 0px 0px 10px 10px; margin-top: 0;">
+    <div class="bar" v-if="progress">
+      <div class="load" :style="'width:'+progress+'%;'"></div>
+    </div>
+    <div class="cardLower" style="border-radius: 0px 0px 10px 10px; margin-top: 0;">
       <div style="display: flex; justify-content: space-between;">
         <div style="display: flex;">
           <p style="margin: 0;" v-html="content"></p>
-        </div>
-        <div style="display: flex;">
-          <!--
-          <button @click="$router.push(link)" class="roundButton">🔍</button>-->
         </div>
       </div>
     </div>
@@ -26,13 +39,30 @@
 </template>
 
 <script>
+import { shell } from 'electron';
+
 export default {
   name: "Card",
   props: {
     title: String,
     subtitle: String,
+    rightTitle: String,
+    rightSubtitle: String,
     content: String,
-    link: String
+    externalLink: String,
+    internalLink: String,
+    styling: String,
+    progress: {
+      validator: function(value) {
+        return !!(typeof value == "number" || typeof value == "string");
+      }
+    },
+  },
+  methods: {
+    openExternalLink(e) {
+      e.preventDefault(); 
+      shell.openExternal(this.externalLink);
+    }
   }
 }
 </script>
