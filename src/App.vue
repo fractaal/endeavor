067d45c8016ec66a596d735634d3870ea604bad8
Jude <benedict.jude.rocat@gmail.com>
2020-12-01 01:04:15 +0800

    🖼 Theming and settings tweaks

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app" :class="sharedStore.theme" class="fullPage loginBackground">
+  <div id="app" :class="sharedStore.settings.theme" class="fullPage loginBackground">
     <transition name="transition" mode="out-in">
       <keep-alive>
         <router-view name="default"></router-view>
@@ -27,17 +27,29 @@ console.log("Data path: " + data);
 export default Vue.extend({
   data() {
     return {
-      sharedStore
+      sharedStore,
     };
   },
   async created () {
     // Creating new eLearn object in store...
     this.sharedStore.eLearn = new ELearn();
     this.$router.push('/load');
+
+    this.sharedStore.fullPageLoadText = "Checking for existing config file..."
     
     // Checking if endeavor.json file exists
     if (fs.existsSync(path.join(data, "endeavor.json"))) {
       endeavor = JSON.parse(fs.readFileSync(path.join(data, "endeavor.json"), {encoding: "utf8"}));
+
+      // Filling any missing data on endeavor 
+      for (const key in this.sharedStore.settings) {
+        if (!endeavor[key]) {
+          console.log(`${key} does not exist in pre-existing configuration file, falling back to default`);
+          endeavor[key] = this.sharedStore.settings[key];
+        }
+      }
+
+      this.sharedStore.settings = endeavor.settings;
 
       // Remove any insecure plaintext login information.
       if (endeavor.username) delete endeavor.username;
@@ -46,22 +58,28 @@ export default Vue.extend({
 
       // Attempting an automatic login
       try {
-        this.sharedStore.settings = endeavor.settings;
+        
         let credentials; let credential; let loginResult;
         if (this.sharedStore.settings.saveLogin) {
           try {
+            this.sharedStore.fullPageLoadText = "Logging you in..."
             credentials = await keytar.findCredentials("endeavor");
             credential = credentials[0];
-            loginResult = await this.sharedStore.eLearn.login(credential.account, credential.password);
+            loginResult = await this.sharedStore.eLearn.login(credential.account, credential.password, log => {
+              this.sharedStore.fullPageLoadLog.push(log);
+            });
           } catch(err) {
+            this.sharedStore.fullPageLoadText = "❌ Login failed!"
             console.warn("An error occured trying to retrieve login credentials: " + err);
             this.$router.push('/login');
           }
           
           if (loginResult) {
+            this.sharedStore.fullPageLoadText = "Welcome!"
             console.log("Autologin success");
             this.$router.push("/home");
           } else {
+            this.sharedStore.fullPageLoadText = "❌ Login failed!"
             console.warn("Automatic login failed!");
             this.$router.push('/login');
           }
@@ -70,6 +88,7 @@ export default Vue.extend({
           this.$router.push('/login');
         }
       } catch(err) {
+        this.sharedStore.fullPageLoadText = "❌ App preference load error!"
         console.warn("An error occurred trying to initialize app preferences." + err);
         this.$router.push('/login');
       }
