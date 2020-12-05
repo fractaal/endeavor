aad6ceba1520088797a69cf5bba04e395467d198
Jude <benedict.jude.rocat@gmail.com>
2020-12-05 14:42:20 +0800

    ✨ Modified grade UI layout

--- src/components/Grade.vue
+++ src/components/Grade.vue
@@ -1,9 +1,9 @@
 <template>
   <div style="display: flex; flex-direction:column">
-    <h4 class="nospacing" style="font-weight: 200; margin-bottom: -10px;">GRADE</h4>
+    <h4 class="nospacing" style="font-weight: 300; margin-bottom: -10px;">GRADE</h4>
     <div style="display: flex; align-items: flex-end;">
-      <h1 class="nospacing" style="margin: 0px 0px -10px 0px; font-size: 64px;">{{formattedgrade}}</h1>
-      <h1 class="nospacing" style="font-weight: 200;">/100</h1>
+      <h1 class="nospacing" style="margin: 0px 0px -10px 0px; font-size: 48px;">{{formattedgrade}}</h1>
+      <h1 class="nospacing" style="font-weight: 200;">/?</h1>
     </div>
   </div>
 </template>
