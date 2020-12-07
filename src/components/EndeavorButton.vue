b59907308deb44e687964b6bd8cb680035b03c7e
Jude <benedict.jude.rocat@gmail.com>
2020-12-07 21:57:19 +0800

    ➕ Create new EndeavorButton component!

---
+++ src/components/EndeavorButton.vue
@@ -0,0 +1,18 @@
+<template>
+  <button @click="callback" class="buttonwithlabel"><slot></slot></button>
+</template>
+
+<script>
+export default {
+  name: "EndeavorButton",
+  methods: {
+    callback(e) {
+      this.$emit("click", e);
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
