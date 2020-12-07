c4a6ec6864335716d689998aa18f896a1c6a172d
Jude <benedict.jude.rocat@gmail.com>
2020-12-08 02:40:25 +0800

    ✨ Functional grade component

--- src/components/Grade.vue
+++ src/components/Grade.vue
@@ -3,22 +3,26 @@
     <h4 class="nospacing" style="font-weight: 300; margin-bottom: -10px;">GRADE</h4>
     <div style="display: flex; align-items: flex-end;">
       <h1 class="nospacing" style="margin: 0px 0px -10px 0px; font-size: 48px;">{{formattedgrade}}</h1>
-      <h1 class="nospacing" style="font-weight: 200;">/?</h1>
+      <h1 class="nospacing" style="font-weight: 200;">/{{maxGrade}}</h1>
     </div>
   </div>
 </template>
 
 <script>
+
 export default {
   name: "Grade",
   props: {
+    maxGrade: Number,
     grade: Number,
+    modname: String,
+    id: Number,
   },
   computed: {
     formattedgrade: function() {
-      return this.grade.toFixed(0);
+      return Number(this.grade).toFixed(0);
     }
-  }
+  },
 }
 </script>
 
