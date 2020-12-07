b7ee9f1cd5f7fe97f9e94f418de9d1eefd233ddb
Jude <benedict.jude.rocat@gmail.com>
2020-12-07 21:59:44 +0800

    ➕ Changelog

--- src/changelog.ts
+++ src/changelog.ts
@@ -1,14 +1,14 @@
 export default [
   {
-    title: "✨ Quality of Life Update #3",
+    title: "✨ Quality of Life Update #4",
     text: "Small (but hopefully helpful) tweaks to Endeavor."
   },
   {
-    title: "✨ Hidden courses!",
-    text: "Hide courses that you're not actively working on!"
+    title: "✨ Improved button UX!",
+    text: "Buttons now feel better. 🤷‍♂️"
   },
   {
-    title: "📐 Card layout changes",
-    text: "Very miniscule, but I hope it helps a tiny bit."
+    title: "🖼 Pictures!",
+    text: "Endeavor now displays pictures embedded in Moodle responses, including your profile picture! (experimental!)"
   },
 ]
