b66fa70237e389ff9ae26b9af187a68f61170c0b
Jude <benedict.jude.rocat@gmail.com>
2020-12-17 20:42:56 +0800

    ✨Change styling

--- src/components/TableOfContents.vue
+++ src/components/TableOfContents.vue
@@ -1,8 +1,8 @@
 <template>
-  <div style="display: block; margin: 20px 10px; max-height: 66vh; overflow-y: auto; overflow-x: hidden;">
-    <h3 class="nospacing" style="font-weight: 300;">OUTLINE</h3>
-    <div v-for="(item, index) in list" :key="item">
-      <EndeavorButton :styling="activeitem == index ? 'active' : ''" @click="emitNavigationEvent(index)">{{index+1}}. {{item}}</EndeavorButton>
+  <div style="display: block; margin: 20px 10px; overflow-x: hidden;">
+    <h3 class="nospacing" style="font-weight: 300; margin: 10px;">OUTLINE</h3>
+    <div style="max-height: 60vh; overflow-y: auto; overflow-x: hidden;">
+      <EndeavorButton v-for="(item, index) in list" :key="item" :styling="activeitem == index ? 'active' : ''" @click="emitNavigationEvent(index)">{{index+1}}. {{item}}</EndeavorButton>
     </div>
   </div>
 </template>
