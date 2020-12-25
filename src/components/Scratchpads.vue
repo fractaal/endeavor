29d21780f2f44854b82b89840d695a31c61a19c1
Jude <benedict.jude.rocat@gmail.com>
2020-12-25 19:46:43 +0800

    ➕ Scratchpads component that handles multiple scratchpads

---
+++ src/components/Scratchpads.vue
@@ -0,0 +1,46 @@
+<template>
+  <div style="position: fixed; pointer-events: none; width: 100vw; height: 100vh;">
+    <transition-group name="modal">
+      <floating-editor v-for="scratchpad in scratchpads" :key="scratchpad.id"
+        :show="true"
+        :title="scratchpad.name"
+        :focused="scratchpad.focused"
+        v-model="scratchpad.content"
+        @close="closeScratchpad(scratchpad.id)"
+        style="pointer-events: auto !important;"
+        :style="scratchpad.focused ? 'z-index: 10' : 'z-index: 1'"
+        @editor-clicked="focusScratchpad(scratchpad.id)"
+      />
+    </transition-group>
+  </div>
+</template>
+
+<script>
+import { openScratchpad, closeScratchpad, toggleScratchpad, focusScratchpad } from '../scratchpad/manager';
+import scratchpads from '../scratchpad/manager';
+
+import { Bus } from '../main';
+import FloatingEditor from './FloatingEditor.vue';
+
+export default {
+  name: "Scratchpads",
+  created() {
+  
+    Bus.$on("open-scratchpad", (id, name) => openScratchpad(id, name));
+    Bus.$on("close-scratchpad", id => closeScratchpad(id));
+    Bus.$on("toggle-scratchpad", (id, name) => toggleScratchpad(id, name));
+  },
+  components: {
+    FloatingEditor  
+  },
+  data() {
+    return {
+      scratchpads,
+    };
+  },
+  methods: {
+    closeScratchpad: id => closeScratchpad(id),
+    focusScratchpad: id => focusScratchpad(id),
+  },
+};
+</script>
