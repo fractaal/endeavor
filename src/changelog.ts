e1df58bf2c38570e45e892df4e8979434ecf85fb
Jude <benedict.jude.rocat@gmail.com>
2020-12-30 18:58:51 +0800

    ➕ Changelog!

--- src/changelog.ts
+++ src/changelog.ts
@@ -10,5 +10,9 @@ export default [
   {
     title: "🔧 Fix lesson/book view bugs",
     text: "Various problems with lessons and books. Lessons/books will not hang on the first lesson/book viewed anymore, and their UI has been greatly improved."
+  },
+  {
+    title: "📷 Pixelated icons fix?",
+    text: "For a long time now, Endeavor's icon (on Windows, at least) looks pixelated on the taskbar. This patch tries to fix that.",
   }
 ]
