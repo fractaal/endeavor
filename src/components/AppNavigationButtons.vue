31362e6c9ae15191ff70b730b1ab7c7589ea0df3
Jude <benedict.jude.rocat@gmail.com>
2021-01-01 20:55:36 +0800

    ✨ New App Navigation Button component

---
+++ src/components/AppNavigationButtons.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex-row">
+    <button class="navbar-button navbar-button-darkened" @click="$router.go(-1)">
+      <fai icon="arrow-left"/>
+    </button>
+    <button class="navbar-button navbar-button-darkened" @click="$router.go(1)">
+      <fai icon="arrow-right"/>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppNavigationButtons",
+  data() {
+    return {};
+  },
+  props: {},
+  methods: {},
+};
+</script>
