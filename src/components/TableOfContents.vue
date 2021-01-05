d41ce16e7408284d602c4c0643479d40f3235b89
Jude <benedict.jude.rocat@gmail.com>
2021-01-05 22:00:11 +0800

    🔧 Change max-height

--- src/components/TableOfContents.vue
+++ src/components/TableOfContents.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="table-of-contents">
     <h3 class="nospacing" style="font-weight: 300; margin: 0 0 10px 0;">OUTLINE</h3>
-    <div style="max-height: 80vh; overflow-y: auto; overflow-x: hidden;">
+    <div style="max-height: 75vh; overflow-y: auto; overflow-x: hidden;">
       <EndeavorButton style="margin: 5px 0; text-align: left;" class="fill-row" v-for="(item, index) in list" :key="item" :styling="activeitem == index ? 'active' : ''" @click="emitNavigationEvent(index)">
           {{index+1}}. {{item}}
       </EndeavorButton>
