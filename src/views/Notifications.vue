bd904abaa0500e1da603c4eee962a28fee1bf610
Jude <benedict.jude.rocat@gmail.com>
2021-01-07 20:40:18 +0800

    ➕ Add notifications view

---
+++ src/views/Notifications.vue
@@ -0,0 +1,105 @@
+<template>
+  <div>
+    <div class="header">
+      <h1 class="light">NOTIFICATIONS</h1>
+      <endeavor-button @click="sharedStore.eLearn.updateCacheAndNotify()"><fai icon="redo-alt" style="margin-right: 10px;"/>Refresh</endeavor-button>
+    </div>
+    <div class="content">
+      <div class="cardlist">
+        <h2 class="light">JUST IN</h2>
+        <loader text="GETTING IN TOUCH..." v-if="sharedStore.eLearn.notificationsStatus.isLoading"/>
+        <icon-with-text :icon="'exclamation-circle'" v-if="sharedStore.eLearn.notificationsStatus.isFailed">
+          <template v-slot:title>
+            WHOOPS
+          </template>
+          <template v-slot:subtitle>
+            Something happened and notifications failed to load.<br>
+            Perhaps you could try again?
+          </template>
+        </icon-with-text>
+        <icon-with-text 
+        :icon="'check-circle'" 
+        v-if="!sharedStore.eLearn.notificationsStatus.isLoading && !sharedStore.eLearn.notificationsStatus.isFailed && sharedStore.eLearn.notifications.length == 0">
+          <template v-slot:title>
+            NOTHING FOR TODAY
+          </template>
+          <template v-slot:subtitle>
+            No new stuff today!
+          </template>
+        </icon-with-text>
+        <h2 class="centered" ></h2>
+        <card v-for="notif in sharedStore.eLearn.notifications" 
+        :key="notif.title" 
+        >
+          <template v-slot:header>
+            <div class="flex-row">
+              <fai icon="plus-circle" size="2x" v-if="notif.type == 'ADD'"/>
+              <fai icon="dot-circle" size="2x" v-else-if="notif.type == 'UPDATE'"/>
+              <fai icon="minus-circle" size="2x" v-else/>
+              <h2 class="light nospacing">{{notif.title}}</h2>
+            </div>
+            <p class="light nospacing">{{notif.text}}</p>
+          </template>
+          <template v-slot:buttons>
+            <endeavor-button v-if="notif.courseId && notif.sectionId && notif.moduleId" @click="$router.push(`/modules/${notif.courseId}/${notif.moduleId}`)">
+              🔍 Open Module 
+            </endeavor-button>
+            <endeavor-button v-else-if="notif.courseId && notif.sectionId" @click="$router.push(`/home/courses/${notif.courseId}/${notif.sectionId}`)">
+              🔍 Open Section 
+            </endeavor-button>
+            <endeavor-button v-else-if="notif.courseId" @click="$router.push(`/home/courses/${notif.courseId}`)">
+              🔍 Open Course
+            </endeavor-button>
+          </template>
+        </card>
+        <hr>
+        <h2 class="light">ARCHIVE</h2>
+        <card v-for="notif in sharedStore.eLearn.notificationsArchive" 
+        :key="notif.title" 
+        >
+          <template v-slot:header>
+            <div class="flex-row">
+              <fai icon="plus-circle" size="2x" v-if="notif.type == 'ADD'"/>
+              <fai icon="dot-circle" size="2x" v-else-if="notif.type == 'UPDATE'"/>
+              <fai icon="minus-circle" size="2x" v-else/>
+              <h2 class="light nospacing">{{notif.title}}</h2>
+            </div>
+            <p class="light nospacing">{{notif.text}}</p>
+          </template>
+          <template v-slot:buttons>
+            <endeavor-button v-if="notif.courseId && notif.sectionId && notif.moduleId" @click="$router.push(`/modules/${notif.courseId}/${notif.moduleId}`)">
+              🔍 Open Module 
+            </endeavor-button>
+            <endeavor-button v-else-if="notif.courseId && notif.sectionId" @click="$router.push(`/home/courses/${notif.courseId}/${notif.sectionId}`)">
+              🔍 Open Section 
+            </endeavor-button>
+            <endeavor-button v-else-if="notif.courseId" @click="$router.push(`/home/courses/${notif.courseId}`)">
+              🔍 Open Course
+            </endeavor-button>
+          </template>
+        </card>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import sharedStore from '../store';
+
+import Card from '@/components/Card.vue';
+import EndeavorButton from '@/components/EndeavorButton.vue';
+import Loader from '@/components/Loader.vue';
+import IconWithText from '@/components/IconWithText.vue';
+
+export default {
+  components: { Card, EndeavorButton, Loader, IconWithText },
+  name: "Notifications",
+  data() {
+    return {
+      sharedStore,
+    };
+  },
+  props: {},
+  methods: {},
+};
+</script>
