dd4360cfc030b33485e58d5fbb78b3bbf59b5e43
Jude <benedict.jude.rocat@gmail.com>
2021-01-09 21:04:15 +0800

    ➕ Changelog!

--- src/changelog.ts
+++ src/changelog.ts
@@ -4,7 +4,11 @@ export default [
     text: "Sleeker, cleaner, and faster!",
   },
   {
-    title: "🔁 Auto-retry login",
-    text: "Previously, if Endeavor hung on login, it would just stay there forever. Not anymore! It'll retry for a max of 5 retries."
+    title: "🔻 Download buttons to attachments",
+    text: "You can now download attachments directly from Endeavor!"
+  },
+  {
+    title: "🔧 Changed the position of back/forward buttons!",
+    text: "The sidebar felt a little crowded."
   }
 ]
