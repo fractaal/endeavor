529b6e0cdadc57c0474e812ce75d26b5334fe80f
fractaal <sonoroustwo@gmail.com>
2021-02-11 02:47:29 +0800

    ⚙ reactive time changelog!

--- src/changelog.ts
+++ src/changelog.ts
@@ -1,18 +1,6 @@
 export default [
   {
-    title: "🔥 User Interface Overhaul",
-    text: "Sleeker, cleaner, and faster!",
-  },
-  {
-    title: "🔻 Download buttons to attachments",
-    text: "You can now download attachments directly from Endeavor!"
-  },
-  {
-    title: "🔧 Changed the position of back/forward buttons!",
-    text: "The sidebar felt a little crowded."
-  },
-  {
-    title: "🔧 Fixed a bug where the Timeline doesn't show up",
-    text: "It was caused by the Timeline automatically assuming all events were tied to courses, meaning it would hang on events that weren't tied to courses."
+    title: "⌚ Reactive Time",
+    text: "Time distances (e.g. \"about 2 hours ago\") now automatically update!",
   }
 ]
