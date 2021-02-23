da6e2789f6c9dfef6f94afab93f51fb6645cc3da
fractaal <sonoroustwo@gmail.com>
2021-02-23 11:19:40 +0800

    ✨Changelog!

--- src/changelog.ts
+++ src/changelog.ts
@@ -1,6 +1,21 @@
 export default [
   {
-    title: "⌚ Reactive Time",
-    text: "Time distances (e.g. \"about 2 hours ago\") now automatically update!",
+    title: "🧭 Navigation Overhaul",
+    text: "Knowing where you are was a bit difficult to do in Endeavor compared to eLearn. Not anymore!"
+  },
+  {
+    title: "📱 UI Change",
+    text: "The navigation overhaul merited a change in the user interface! The top bar is back, but it's sleek. You won't even know it's there!"
+  },
+  {
+    title: "🔧 Bug Fixes",
+    text: "Some backend bug fixes were made. I probably made them worse, though. Sorry in advance."
+  },
+  {
+    title: "🔺 High CPU usage temporary fix",
+    text: "The overdue animation (pulsing white border) caused high CPU usage, and is disabled. I'm thinking of an alternative panic animation, but for now, it's gone."
   }
-]
+] as {
+  title: string;
+  text: string;
+}[]
