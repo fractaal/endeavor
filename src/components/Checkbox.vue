d961e0cbf771f748706ddd65e8d06155e8aaca76
fractaal <sonoroustwo@gmail.com>
2021-02-26 21:39:44 +0800

    ✨ new checkbox component

---
+++ src/components/Checkbox.vue
@@ -0,0 +1,25 @@
+<template>
+  <button style="margin-right: 10px;" class="roundButton" @click.prevent="updateValue">{{value ? '✔' : '❌'}}</button>
+</template>
+
+<script>
+export default {
+  name: "Checkbox",
+  props: {
+    value: {
+      type: Boolean,
+      required: false,
+    }
+  },
+  methods: {
+    updateValue: function() {
+      console.log(this.value + " to " + !this.value)
+      this.$emit("input", !this.value)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
