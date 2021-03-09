c6058dff61ad2fb80371880e3d25e2925d2a1227
fractaal <sonoroustwo@gmail.com>
2021-03-09 10:19:42 +0800

    ✨show loading animation

--- src/components/Grade.vue
+++ src/components/Grade.vue
@@ -1,10 +1,16 @@
 <template>
   <div style="display: flex; flex-direction:column">
-    <h4 class="nospacing" style="font-weight: 300; margin-bottom: -10px;">GRADE</h4>
-    <div style="display: flex; align-items: flex-end;">
-      <h1 class="nospacing" style="margin: 0px 0px -10px 0px; font-size: 48px;">{{formattedgrade}}</h1>
-      <h1 class="nospacing" style="font-weight: 200;">/{{maxGrade}}</h1>
-    </div>
+    <h4 class="nospacing" style="font-weight: 300; margin-bottom: -5px;">GRADE</h4>
+    <transition name="transition">
+      <div v-if="isLoading" style="display: flex; align-items: flex-end;">
+        <h1 class="shimmer nospacing" style="margin: 0px 0px -10px 0px; font-size: 48px;">?</h1>
+        <h1 class="shimmer nospacing" style="font-weight: 200;">/?</h1>
+      </div>
+      <div v-else style="display: flex; align-items: flex-end;">
+        <h1 class="nospacing" style="margin: 0px 0px -10px 0px; font-size: 48px;">{{formattedgrade}}</h1>
+        <h1 class="nospacing" style="font-weight: 200;">/{{maxGrade}}</h1>
+      </div>
+    </transition>
   </div>
 </template>
 
@@ -17,13 +23,14 @@ export default {
     grade: Number,
     modname: String,
     id: Number,
+    isLoading: Boolean,
   },
   computed: {
     formattedgrade: function() {
-      return Number(this.grade).toFixed(0);
+      return parseInt(this.grade).toFixed(0);
     }
   },
-}
+};
 </script>
 
 <style>
