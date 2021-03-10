5731cc19c6f62d9cdc3593a31daf8df2529ccd64
fractaal <sonoroustwo@gmail.com>
2021-03-10 11:21:50 +0800

    ✨ toasts!

---
+++ src/components/ToastProvider.vue
@@ -0,0 +1,17 @@
+<template>
+  <div style="position: absolute;"></div>
+</template>
+
+<script>
+import {Bus} from '../main';
+
+export default {
+  name: "ToastProvider",
+  created() {
+    Bus.$on('toast', (content, options, type) => {
+      if (type) this.$toast[type](content, options);
+      else this.$toast(content, options);
+    })
+  }
+}
+</script>
